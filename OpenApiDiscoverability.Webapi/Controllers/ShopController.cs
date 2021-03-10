681822f96042412112e4d4ff522f6d8504368fc8
yterraillon <yterraillon@gmail.com>
2021-03-10 10:53:30 +0100

    Clean solution

--- OpenApiDiscoverability.Webapi/Controllers/ShopController.cs
+++ OpenApiDiscoverability.Webapi/Controllers/ShopController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace OpenApiDiscoverability.Webapi.Controllers
 {
     [Route("api/[controller]")]
